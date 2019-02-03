5f8190d48bafca70dea03079939b9a06da6388b2
cagdaseksi <cagdas.eksi@hotmail.com>
2019-02-03 23:46:59 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import {
+    StyleSheet,
+    TouchableHighlight,
+    Dimensions,
+    View,
+    Text,
+    Linking
+} from 'react-native';
+
+import Pdf from 'react-native-pdf';
+
+const WIN_WIDTH = Dimensions.get('window').width;
+const WIN_HEIGHT = Dimensions.get('window').height;
+//const links = [ 'http://samples.leanpub.com', 'https://google.com' ];
+
+var numbers1 = [
+    { page: 1, link : 'link1' },
+    { page: 2, link : 'link3' },
+    { page: 3, link : 'link4' },
+];
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: 1,
+            scale: 1,
+            numberOfPages: 0,
+            horizontal: false,
+            width: WIN_WIDTH,
+            link: ''
+        };
+        this.pdf = null;
+    }
+
+    prePage = () => {
+        let prePage = this.state.page > 1 ? this.state.page - 1 : 1;
+        this.setState({page: prePage});
+        this.findLink(prePage)
+        console.log(`prePage: ${prePage}`);
+    };
+
+    nextPage = () => {
+        let nextPage = this.state.page + 1 > this.state.numberOfPages ? this.state.numberOfPages : this.state.page + 1;
+        this.setState({page: nextPage});
+        this.findLink(nextPage)
+        console.log(`nextPage: ${nextPage}`);
+    };
+
+    zoomOut = () => {
+        let scale = this.state.scale > 1 ? this.state.scale / 1.2 : 1;
+        this.setState({scale: scale});
+        console.log(`zoomOut scale: ${scale}`);
+    };
+
+    zoomIn = () => {
+        let scale = this.state.scale * 1.2;
+        scale = scale > 3 ? 3 : scale;
+        this.setState({scale: scale});
+        console.log(`zoomIn scale: ${scale}`);
+    };
+
+    findLink = (index) => {
+        var numbers = [
+            { page: 0, link : 'link0' },
+            { page: 1, link : 'link1' },
+            { page: 2, link : 'link2' },
+            { page: 3, link : 'link3' },
+            { page: 4, link : 'link4' }
+        ];
+        console.log(`findLink: ${index}`);
+
+        var text = `${numbers[index].link}`
+
+        this.setState({link: text});
+
+    };
+
+    switchHorizontal = () => {
+        this.setState({horizontal: !this.state.horizontal, page: this.state.page});
+    };
+
+    render() {
+        let source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
+        //let source = require('./test.pdf');  // ios only
+        //let source = {uri:'bundle-assets://test.pdf'};
+
+        //let source = {uri:'file:///sdcard/test.pdf'};
+        //let source = {uri:"data:application/pdf;base64,"};
+
+        return (
+            <View style={styles.container}>
+                <View style={{flexDirection: 'row', height:30, marginTop: 21}}>
+                    <TouchableHighlight disabled={this.state.page === 1}
+                                        style={this.state.page === 1 ? styles.btnDisable : styles.btn}
+                                        onPress={() => this.prePage()}>
+                        <Text style={styles.btnText}>{'-'}</Text>
+                    </TouchableHighlight>
+                    <View style={styles.btnText}><Text style={styles.btnText}>Page {this.state.page}</Text></View>
+                    <TouchableHighlight disabled={this.state.page === this.state.numberOfPages}
+                                        style={this.state.page === this.state.numberOfPages ? styles.btnDisable : styles.btn}
+                                        onPress={() => this.nextPage()}>
+                        <Text style={styles.btnText}>{'+'}</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight disabled={this.state.scale === 1}
+                                        style={this.state.scale === 1 ? styles.btnDisable : styles.btn}
+                                        onPress={() => this.zoomOut()}>
+                        <Text style={styles.btnText}>{'-'}</Text>
+                    </TouchableHighlight>
+                    <View style={styles.btnText}><Text style={styles.btnText}>Scale</Text></View>
+                    <TouchableHighlight disabled={this.state.scale >= 3}
+                                        style={this.state.scale >= 3 ? styles.btnDisable : styles.btn}
+                                        onPress={() => this.zoomIn()}>
+                        <Text style={styles.btnText}>{'+'}</Text>
+                    </TouchableHighlight>
+                    {/* <View style={styles.btnText}><Text style={styles.btnText}>{!this.state.horizontal ? (<Text style={styles.btnText}>{'Yatay'}</Text>) : (
+                            <Text style={styles.btnText}>{'Dikey'}</Text>)}</Text></View> */}
+                    <TouchableHighlight style={styles.btn} onPress={() => this.switchHorizontal()}>
+                        {!this.state.horizontal ? (<Text style={styles.btnText}>{'Yatay'}</Text>) : (
+                            <Text style={styles.btnText}>{'Dikey'}</Text>)}
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.btn} onPress={ ()=>{ Linking.openURL('https://google.com')}} >
+                        <Text style={styles.btnText}>{this.state.link}</Text>
+                    </TouchableHighlight>
+                </View>
+                <View style={{flex:1,width: this.state.width}}>
+                    <Pdf ref={(pdf) => {
+                        this.pdf = pdf;
+                    }}
+                         source={source}
+                         page={this.state.page}
+                         scale={this.state.scale}
+                         horizontal={this.state.horizontal}
+                         onLoadComplete={(numberOfPages, filePath,{width,height},tableContents) => {
+                             this.state.numberOfPages = numberOfPages; //do not use setState, it will cause re-render
+                             console.log(`total page count: ${numberOfPages}`);
+                             console.log(tableContents);
+
+                         }}
+                         onPageChanged={(page, numberOfPages) => {
+                             this.state.page = page; //do not use setState, it will cause re-render
+                             console.log(`current page: ${page}`);
+                             //this.setState({page: page});
+                         }}
+                         onError={(error) => {
+                             console.log(error);
+                         }}
+                         style={{flex:1}}
+                         />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 25,
+        backgroundColor: '#f8f8f8'
+    },
+    btn: {
+        margin: 2,
+        padding: 2,
+        backgroundColor: "aqua",
+    },
+    btnDisable: {
+        margin: 2,
+        padding: 2,
+        backgroundColor: "gray",
+    },
+    btnText: {
+        margin: 2,
+        padding: 2,
+    }
+});
---
+++ src/components/header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize:30,
+        textAlign:'center',
+        fontWeight:'bold'
+    },
+    viewStyle: {
+        backgroundColor: '#f8f8f8',
+        height:70,
+        justifyContent:'center',
+        textAlign:'center',
+        marginTop:30,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+
+        elevation: 7,
+    }
+});
+
+export default class Header extends Component {
+  render() {
+    return (
+      <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>{this.props.text}</Text>
+      </View>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+//AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
